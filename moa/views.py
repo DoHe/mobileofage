8a461b0a2d8de086a3794cbb916db8b08941088f
dohe <wahlholz@gmx.de>
2019-12-14 21:41:44 +0100

    Default to main page

--- moa/views.py
+++ moa/views.py
@@ -8,14 +8,13 @@ class Index(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        comic = self.request.GET.get('c')
-        if comic:
-            image, alt, title, prev, nxt = scrape_comic(comic)
-            context['url'] = comic
-            context['image'] = image
-            context['alt'] = alt
-            context['title'] = title
-            context['previous'] = '/?c=' + prev
-            if nxt:
-                context['next'] = '/?c=' + nxt
+        comic = self.request.GET.get('c', 'http://www.dumbingofage.com/')
+        image, alt, title, prev, nxt = scrape_comic(comic)
+        context['url'] = comic
+        context['image'] = image
+        context['alt'] = alt
+        context['title'] = title
+        context['previous'] = '/?c=' + prev
+        if nxt:
+            context['next'] = '/?c=' + nxt
         return context
